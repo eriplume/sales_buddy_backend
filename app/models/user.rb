b1e6adea9fb8362175c1754ed1a0ab6246a221ee
eriplume <eripi840@gmail.com>
2023-12-08 10:04:42 +0900

    :recycle: Fix: rubocop修正 #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   belongs_to :group
 
   validates :line_id, presence: true
-  validates :group_id, presence: true
   validates :notifications, inclusion: { in: [true, false] }
 
   enum role: { general: 0, leader: 1, admin: 2 }

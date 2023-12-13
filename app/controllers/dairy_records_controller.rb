5a8ff21c4d4fdc43341482dcb648f6ca9eb8c4e6
eriplume <eripi840@gmail.com>
2023-12-13 12:28:37 +0900

    :recycle: Update: コントローラ修正 #4

--- app/controllers/dairy_records_controller.rb
+++ app/controllers/dairy_records_controller.rb
@@ -6,21 +6,10 @@ class DairyRecordsController < ApplicationController
   end
 
   def create
-    @dairy_record = DairyRecord.new(record_params)
-            
-    if @dairy_record.save
-      if params[:dairy_record][:customers]
-        customer_ids_counts = params[:dairy_record][:customers].tally
-        # 渡ってくる配列[1, 2, 1]に対して
-        # tally によって生成されるハッシュ { 1 => 2, 2 => 1 }
-        customer_ids_counts.each do |customer_type_id, count|
-        # 上のそれぞれを|customer_type_id, count|に割り当てる。それに対して下記の処理をする。
-        customer_type = CustomerType.find(customer_type_id)
-        # CustomerTypeテーブルの(customer_type_id)カラムから一致するものを探してそれを今回のcustomer_typeとする
-        CustomerRecord.create(dairy_record: @dairy_record, customer_type: customer_type, count: count)
-        end
-      end
-      
+    @dairy_record = DairyRecord.new(dairy_record_params)
+    service = CustomerRecordCreateService.new(@dairy_record, params[:customer_counts])
+
+    if service.call
       render json: @dairy_record, status: :created
     else
       render json: @dairy_record.errors, status: :unprocessable_entity
@@ -29,7 +18,7 @@ class DairyRecordsController < ApplicationController
 
   private
       
-  def record_params
+  def dairy_record_params
     params.require(:dairy_record).permit(:total_amount, :total_number, :count, :date, :user_id)
   end
 end

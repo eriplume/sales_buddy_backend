1099128f1fc898b308dd300882537be95f6e7485
eriplume <eripi840@gmail.com>
2023-12-13 13:06:12 +0900

    :zap: Update: response内容修正 #4

--- app/controllers/dairy_records_controller.rb
+++ app/controllers/dairy_records_controller.rb
@@ -10,7 +10,10 @@ class DairyRecordsController < ApplicationController
     service = CustomerRecordCreateService.new(@dairy_record, params[:customer_counts])
 
     if service.call
-      render json: @dairy_record, status: :created
+      render json: {
+        status: 'success', 
+        dairy_record: { '登録する日': @dairy_record.date, '合計金額': @dairy_record.total_amount, 'セット率': @dairy_record.set_rate, '客単価': @dairy_record.average_spend }
+      }
     else
       render json: @dairy_record.errors, status: :unprocessable_entity
     end

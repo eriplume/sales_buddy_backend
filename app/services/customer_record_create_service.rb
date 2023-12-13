3d68f350f530ddfa9f1f3486008b81058bbd4277
eriplume <eripi840@gmail.com>
2023-12-13 12:27:31 +0900

    :recycle: Update: サービスクラスの作成 #4

---
+++ app/services/customer_record_create_service.rb
@@ -0,0 +1,17 @@
+class CustomerRecordCreateService
+  def initialize(dairy_record, customer_counts)
+    @dairy_record = dairy_record
+    @customer_counts = customer_counts
+  end
+  
+  def call
+    ActiveRecord::Base.transaction do
+      @dairy_record.save! 
+      @customer_counts.each do |customer_type_id, count|
+        customer_type = CustomerType.find(customer_type_id)
+        CustomerRecord.create!(dairy_record: @dairy_record, customer_type: customer_type, count: count)
+      end
+    end
+  end
+end
+  

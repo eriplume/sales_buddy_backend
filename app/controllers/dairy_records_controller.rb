b8210adfa1d13789a6e9b0cd8abd15dee523a4a3
eriplume <eripi840@gmail.com>
2023-12-13 16:00:23 +0900

    :zap: Update: response内容修正 #4

--- app/controllers/dairy_records_controller.rb
+++ app/controllers/dairy_records_controller.rb
@@ -12,7 +12,7 @@ class DairyRecordsController < ApplicationController
     if service.call
       render json: {
         status: 'success', 
-        dairy_record: { '登録する日': @dairy_record.date, '合計金額': @dairy_record.total_amount, 'セット率': @dairy_record.set_rate, '客単価': @dairy_record.average_spend }
+        dairy_record: { date: @dairy_record.date }
       }
     else
       render json: @dairy_record.errors, status: :unprocessable_entity

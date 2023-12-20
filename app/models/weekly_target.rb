8309c4d8a865d629d0d810b282dac78b6451d37b
eriplume <eripi840@gmail.com>
2023-12-20 19:56:54 +0900

    :bug: Fix: バリデーション修正 #6

--- app/models/weekly_target.rb
+++ app/models/weekly_target.rb
@@ -2,7 +2,7 @@ class WeeklyTarget < ApplicationRecord
   belongs_to :user
 
   validates :target, presence: true,
-                    numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 200 }
+                    numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 2000000 }
   validates :start_date, presence: true, uniqueness: { scope: :user_id }
   validates :end_date, presence: true
 end

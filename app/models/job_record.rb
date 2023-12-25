12e583988cac1e8b2aa90a62631f665e1d5f8c0b
eriplume <eripi840@gmail.com>
2023-12-26 02:58:13 +0900

    :sparkles: Feat: job_record登録機能作成 #17

--- app/models/job_record.rb
+++ app/models/job_record.rb
@@ -4,4 +4,15 @@ class JobRecord < ApplicationRecord
 
   validates :date, presence: true
   validates :job_id, uniqueness: { scope: %i[user_id date] }
+
+  def self.create_from_job_names(user_id, date, job_names)
+    transaction do
+      job_names.each do |job_name|
+        job = Job.find_or_create_by(name: job_name)
+        record = new(user_id:, job_id: job.id, date:)
+
+        raise ActiveRecord::Rollback unless record.save
+      end
+    end
+  end
 end

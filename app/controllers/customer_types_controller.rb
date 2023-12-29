d18e7ecac9da89d0db7ea18dd37c3a0d365d8109
eriplume <eripi840@gmail.com>
2023-12-29 09:21:52 +0900

    :zap: Update: 客層タイプをid順に並べ替えて返す #19

--- app/controllers/customer_types_controller.rb
+++ app/controllers/customer_types_controller.rb
@@ -1,6 +1,6 @@
 class CustomerTypesController < ApplicationController
   def index
-    customer_types = CustomerType.all
+    customer_types = CustomerType.order(:id)
     options = customer_types.map do |type|
       { value: type.id, label: type.name }
     end

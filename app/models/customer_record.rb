1ff0b6ac477dba708bc59c111cfebae321872906
eriplume <eripi840@gmail.com>
2023-12-29 09:22:47 +0900

    :recycle: Fix: Lint修正 #19

--- app/models/customer_record.rb
+++ app/models/customer_record.rb
@@ -4,7 +4,7 @@ class CustomerRecord < ApplicationRecord
 
   def self.summary_by_user(user_id)
     CustomerRecord.joins(:dairy_record)
-                  .where(dairy_records: { user_id: user_id })
+                  .where(dairy_records: { user_id: })
                   .group(:customer_type_id)
                   .sum(:count)
   end

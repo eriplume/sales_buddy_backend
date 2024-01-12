7149fd7a8342907274e602216eb395bcf4b7cc7c
eriplume <eripi840@gmail.com>
2024-01-12 22:56:06 +0900

    :sparkles: Feat: 月間レポート編集機能 #32

--- app/controllers/monthly_reports_controller.rb
+++ app/controllers/monthly_reports_controller.rb
@@ -1,8 +1,10 @@
 class MonthlyReportsController < ApplicationController
+  before_action :set_monthly_report, only: [:update]
+
   def index
     user_id = request.headers['user']
     @monthly_reports = MonthlyReport.where(user_id:)
-    render json: @monthly_reports.as_json(only: %i[content month])
+    render json: @monthly_reports.as_json(only: %i[id content month])
   end
 
   def create
@@ -14,9 +16,25 @@ class MonthlyReportsController < ApplicationController
     end
   end
 
+  def update
+    if @monthly_report.update(monthly_report_params)
+      render json: @monthly_report
+    else
+      render json: @monthly_report.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def report_params
     params.require(:monthly_report).permit(:content, :month, :user_id)
+  end
+
+  def monthly_report_params
+    params.require(:monthly_report).permit(:content, :id)
+  end
+
+  def set_monthly_report
+    @monthly_report = MonthlyReport.find(params[:id])
   end
 end

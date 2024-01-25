a10be739c21397780fe153f635dfffaa500dd1a8
eriplume <eripi840@gmail.com>
2024-01-25 14:14:52 +0900

    :sparkles: Feat: タスクの削除ができる #40

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[update destroy]
+
   def index
     user_id = @current_user.id
     group_id = @current_user.group_id
@@ -28,7 +30,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       render json: @task
     else
@@ -36,7 +37,15 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task.destroy
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :is_group_task, :importance, :deadline, :user_id, :group_id)

d576bc109b4741f5ba91fa42804426b6139a3646
eriplume <eripi840@gmail.com>
2024-02-07 15:56:38 +0900

    :sparkles: Feat: commentモデルの追加 #51

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,8 @@ class Task < ApplicationRecord
   belongs_to :group
   belongs_to :user
   belongs_to :completed_by, class_name: 'User', optional: true, inverse_of: :completed_tasks
-
+  has_many :comments, dependent: :destroy
+  
   validates :title, presence: true, length: { maximum: 20 }
   validates :is_group_task, inclusion: { in: [true, false] }
   validates :deadline, presence: true

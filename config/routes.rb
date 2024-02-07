9af7e6ecd25c215ebfc3f1cd13f5573fe7818df1
eriplume <eripi840@gmail.com>
2024-02-07 15:58:28 +0900

    :sparkles: Feat: コメントの投稿, 更新, 削除ができる #51

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     member do
       patch 'complete', to: 'tasks#complete'
     end
+    resources :comments, only: [:create, :update, :destroy]
   end
   resources :groups, only: %i[index create]
   post 'groups/join', to: 'group_memberships#join'

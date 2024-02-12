3925460487e91a2ac9909ab106d44305372e1b07
eriplume <eripi840@gmail.com>
2024-02-12 19:25:38 +0900

    :sparkles: Feat: admin: 売上レコードの一覧取得, 削除機能 #41

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: %i[index update destroy]
     resources :customer_types, only: %i[index update destroy]
+    resources :dairy_records, only: %i[index destroy]
   end
 end

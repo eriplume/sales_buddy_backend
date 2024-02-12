7bdf6f50bca88248e42a368c31ee338d04ac2a29
eriplume <eripi840@gmail.com>
2024-02-12 18:10:10 +0900

    :sparkles: Feat: admin: 客層タイプの編集, 削除機能追加 #41

---
+++ app/controllers/admin/customer_types_controller.rb
@@ -0,0 +1,38 @@
+module Admin
+  class CustomerTypesController < BaseController
+    before_action :set_type, only: %i[update destroy]
+
+    def index
+      customer_types = CustomerType.order(:id)
+      render json: { customer_types: transform_customer_types(customer_types) }
+    end
+
+    def update
+      if @customer_type.update(customer_type_params)
+        render json: @customer_type
+      else
+        render json: @customer_type.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @customer_type.destroy!
+    end
+
+    private
+
+    def set_type
+      @customer_type = CustomerType.find(params[:id])
+    end
+
+    def customer_type_params
+      params.require(:customer_type).permit(:name)
+    end
+
+    def transform_customer_types(customer_types)
+      customer_types.map do |type|
+        type.as_json.transform_keys { |key| key.to_s.camelize(:lower) }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users, only: %i[index update destroy]
+    resources :customer_types, only: %i[index update destroy]
   end
 end
